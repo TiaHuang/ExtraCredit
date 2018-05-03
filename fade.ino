ea324ff3aeac48a762c470875d716dee515f7dac
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-05-03 05:33:22 -0700

    Create fade.ino

---
+++ fade.ino
@@ -0,0 +1,44 @@
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int sensorPin = A0;    // select the input pin for the potentiometer
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  
+  sensorValue = analogRead(sensorPin);
+
+if (sensorValue > 0 && sensorValue <= 255) {
+     analogWrite(5, (1023-sensorValue)/4);
+    digitalWrite(6, HIGH);//LED2 - 2/4
+    digitalWrite(9, HIGH); //LED3 - 1/4
+    sensorValue = analogRead(sensorPin);
+  } else if (sensorValue >= 255 && sensorValue <= 510) {
+    analogWrite(5, (1023-sensorValue)/4);
+    digitalWrite(6, HIGH);//LED2 - 2/4
+    digitalWrite(9, HIGH); //LED3 - 1/4
+    sensorValue = analogRead(sensorPin);
+  } else if (sensorValue >= 510 && sensorValue <= 766) {
+    analogWrite(5, (1023-sensorValue)/4);
+    digitalWrite(6, LOW);//LED2 - 2/4
+    digitalWrite(9, HIGH); //LED3 - 1/4
+    sensorValue = analogRead(sensorPin);
+  } else if (sensorValue >= 766 && sensorValue <= 1023) {
+    analogWrite(5, (1023-sensorValue)/4);
+    digitalWrite(6, LOW);
+    digitalWrite(9, LOW);
+    sensorValue = analogRead(sensorPin);
+  } 
+  
+  delay(sensorValue);
+  Serial.print(sensorValue);}
+  
